ab9675006858abbae16aa6ae1ad8b2eb8bc8d66b
spiedra <jcspiedra@gmail.com>
2021-05-19 10:17:04 -0600

    menu bar ready

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,15 +1,8 @@
 addEventListener('DOMContentLoaded', () => {
-    const nav_menu = document.querySelector('.nav__menu');
     const button_open = document.querySelector('.button-open');
-    const button_close = document.querySelector('.button-close');
+    const nav_menu = document.querySelector('.nav__menu');
 
     button_open.addEventListener('click', () => {
-        // nav_menu.style.display = 'flex';
-        nav_menu.style.top = '60px';
-    })
-
-    button_close.addEventListener('click', () => {
-        // nav_menu.style.display = 'flex';
-         mainMenu.style.top = '-100%';
-    })
+        nav_menu.classList.toggle('active');
+    });
 })

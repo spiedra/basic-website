4e6509aa0b93650fc56341015d7960a1d9208450
spiedra <jcspiedra@gmail.com>
2021-05-26 18:31:43 -0600

    update margin of main

--- assets/js/chooseTours.js
+++
@@ -1,7 +0,0 @@
-addEventListener('DOMContentLoaded', () => {
-    const hola = document.querySelector('.article__main-container');
-
-    hola.addEventListener('click', () => {
-        // alert("jajajja hola");
-    });
-})
